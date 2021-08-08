c151fc3f8915f4127cb0ef33fcbf29cd98ee622e
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-08 16:55:05 +0530

    ADDED: program to retrive all the details of address book from database

--- lib/src/main/java/combridgelabzaddressbook/AddressBookJDBC.java
+++ lib/src/main/java/combridgelabzaddressbook/AddressBookJDBC.java
@@ -2,24 +2,29 @@ package combridgelabzaddressbook;
 
 import java.util.List;
 
-public class AddressBookJDBC {
-    public enum IOService {
+public class AddressBookJDBC
+{
+    public enum IOService
+    {
         DB_IO
     }
 
     private List<Contact> addressBookList;
     private static AddressBookService  addressBookDBService;
 
-    public AddressBookJDBC() {
+    public AddressBookJDBC() 
+    {
         addressBookDBService = AddressBookService.getInstance();
     }
 
-    public AddressBookJDBC(List<Contact> addresBookList) {
+    public AddressBookJDBC(List<Contact> addresBookList) 
+    {
         this();
         this.addressBookList = addressBookList;
     }
 
-    public List<Contact> readAddressBookData(IOService ioservice) throws AddressBookException {
+    public List<Contact> readAddressBookData(IOService ioservice) throws AddressBookException
+    {
         if (ioservice.equals(IOService.DB_IO))
             this.addressBookList = addressBookDBService.readData();
         return this.addressBookList;
