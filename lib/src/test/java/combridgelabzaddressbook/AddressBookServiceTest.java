e58850b49f9fbbe97cb3df08b3ab58439d8f0419
shreyasalanke <85882383+shreyasalanke@users.noreply.github.com>
2021-08-08 17:35:53 +0530

    Update AddressBookServiceTest.java

--- lib/src/test/java/combridgelabzaddressbook/AddressBookServiceTest.java
+++ lib/src/test/java/combridgelabzaddressbook/AddressBookServiceTest.java
@@ -15,8 +15,8 @@ public class AddressBookServiceTest
     @Test
     public void givenAddressBook_WhenUpdate_ShouldSyncWithDB() throws AddressBookException {
         AddressBookJDBC addressBookJDBC = new AddressBookJDBC();
-        addressBookJDBC.updateRecord("Nethaji", "GKW Lauout");
-        boolean result = addressBookJDBC.checkUpdatedRecordSyncWithDatabase("Nethaji");
+        addressBookJDBC.updateRecord("Shreya", "NJP Layout");
+        boolean result = addressBookJDBC.checkUpdatedRecordSyncWithDatabase("Shreya");
         Assert.assertTrue(result);
     }
     @Test
